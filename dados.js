4542383e58c60c3d81be5762e43d8b6b09b97844
Ariane Costa <134095652+Ariane2023@users.noreply.github.com>
2024-09-06 15:35:45 -0400

    Ajustes 2

--- dados.js
+++ dados.js
@@ -4,15 +4,19 @@ let dados = [
         descricao : " Essa é uma habilidade fundamental que pode transformar sua vida. Ao invés de simplesmente memorizar informações, você desenvolve a capacidade de entender conceitos, resolver problemas e aplicar o conhecimento de forma criativa.",
         atividades: "Exemplos de Atividades: Estudo ativo, Espaçamento, Mapas mentais, Técnica Pomodoro, Ensinar outra pessoa, simulados e testes.",
         dicas : "Dicas: Conheça os principais estilos de aprendizado. Que são Visual, Auditivo e Cinestésico",
-        tags: " como aprender a aprender? eu quero aprender"
+        link: "https://blog.escolaconquer.com.br/como-aprender-a-aprender-7-dicas-para-melhorar-seu-aprendizado/",
+        tags: "como aprender a aprender? como aprender eu quero aprender"
+
     },
+       
 
     {
         titulo : "Estilo Visual",
         descricao : "Alunos visuais aprendem melhor através de imagens, gráficos, diagramas e demonstrações. Eles tendem a lembrar-se de informações quando as visualizam.",
         atividades : "Exemplos de Atividades: Criar mapas mentais, usar flashcards com imagens, assistir a vídeos e apresentações.",
         dicas : "Dicas: Utilize cores e marcadores para destacar informações importantes, faça anotações visuais e use diagramas para entender conceitos complexos.",
-        tags: " estilo visual visual"
+        link: "https://www.hotcourses.com.br/study-abroad-info/once-you-arrive/tipos-de-aprendizado-auditivo-cinestesico-e-visual/#:~:text=Existem%20tr%C3%AAs%20formas%20principais%20de,se%20tornar%20um%20estudante%20melhor.",
+        tags: " estilo visual visual ocular"
     }, 
 
     {
@@ -20,6 +24,7 @@ let dados = [
         descricao: "Alunos auditivos aprendem melhor através da escuta. Eles gostam de ouvir explicações, discussões e palestras.",
         atividades: "Exemplos de Atividades: Gravar aulas, participar de debates, ouvir podcasts e audiobooks.",
         dicas: "Dicas: Estude em voz alta, grave suas próprias explicações e participe de grupos de estudo.",
+        link: "https://www.hotcourses.com.br/study-abroad-info/once-you-arrive/tipos-de-aprendizado-auditivo-cinestesico-e-visual/#:~:text=Existem%20tr%C3%AAs%20formas%20principais%20de,se%20tornar%20um%20estudante%20melhor.",
         tags: "Estilo auditivo auditivo ouvitivo ouvir de ouvido"
     },
     
@@ -28,7 +33,7 @@ let dados = [
         descricao: "Alunos cinestésicos aprendem melhor fazendo e experimentando. Eles gostam de atividades práticas e interativas.",
         atividades: "Exemplos de Atividades: Fazer experimentos, construir modelos, participar de atividades físicas e jogos.",
         dicas: "Dicas: Utilize materiais manipuláveis, participe de atividades práticas e crie projetos.",
+        link: "https://www.hotcourses.com.br/study-abroad-info/once-you-arrive/tipos-de-aprendizado-auditivo-cinestesico-e-visual/#:~:text=Existem%20tr%C3%AAs%20formas%20principais%20de,se%20tornar%20um%20estudante%20melhor.",
         tags: "estilo cinestésico cinestesico hands on pratico prático estilo prático"
     }
 ];
-
